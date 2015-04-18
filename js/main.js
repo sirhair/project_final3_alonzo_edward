39bbd112db767dfdf060494527eb0517b216d58f
Edward Alonzo <edward@thenewe.com>
2015-04-18 04:13:48 -0500

    test for plugins existence in main.js to remove errors

--- js/main.js
+++ js/main.js
@@ -6,24 +6,30 @@ $(document).ready ( function () {
   , "img/entrance.jpg"
   , "img/kegs.jpg"
   ], {duration: 3000, fade: 0});
-  $('#menuContainer').mixItUp();  
-  $(".owl-carousel").owlCarousel({
-    loop:true,
-    margin:10,
-    stagePadding:10,
-    autoplay:true,
-    autoplayTimeout:2500,
-    autoplayHoverPause:true,
-    responsive:{
-      0:{
-        items:1
-      },
-      500:{
-        items:2
+  
+ if(jQuery().mixItUp) {
+    $('#menuContainer').mixItUp();
+  }
+ if(jQuery().owlCarousel) {
+   $(".owl-carousel").owlCarousel({
+      loop:true,
+      margin:10,
+      stagePadding:10,
+      autoplay:true,
+      autoplayTimeout:2500,
+      autoplayHoverPause:true,
+      responsive:{
+        0:{
+          items:1
+        },
+        500:{
+          items:2
+        }
       }
-    }
-  });
-  $("#regForm").validate();
-
+    });
+  }
+ if(jQuery().validate) {
+    $("#regForm").validate();
+  }
 
 });
